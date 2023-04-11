ae164723bb3a2394c8210ba4bb43a4696d1e064b
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-04-11 15:50:07 -0300

    Rest-Spread

---
+++ rest-spread/js/script.js
@@ -0,0 +1,15 @@
+//function showList(empresa, ...clientes) {
+    //console.log(empresa)
+
+    //console.log(clientes)
+//}
+
+//showList("Origamid", "João", "Maria", 10)
+
+const numeros = [1,2,4,29,32,2,45,3];
+
+console.log(Math.max(...numeros))
+
+const items = document.querySelectorAll("li");
+
+console.log(items)
