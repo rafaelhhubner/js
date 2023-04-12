28b08449f6e9f945fd11b527f23d182d7b5ae1bb
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-04-12 19:53:31 -0300

    JSON

---
+++ estudos/json/js/script.js
@@ -0,0 +1,39 @@
+const objs = [
+    {
+        nome: 'Matheus',
+        idade: 30,
+        esta_trabalhando: true,
+        detalhes_profissao: {
+            profissao: 'Programador',
+            empresa: 'Empresa X'
+        },
+        hobbies: ['Programar', 'Correr', 'Ler']
+    },
+    {
+        nome: 'João',
+        idade: 25,
+        esta_trabalhando: false,
+        detalhes_profissao: {
+            profissao: null,
+            empresa: null
+        },
+        hobbies: ['Jogar', 'Academia'],
+    }
+]
+
+//JSON
+// converter objeto para json
+const jsonData = JSON.stringify(objs)
+
+console.log(jsonData)
+console.log(typeof jsonData)
+
+// converter json para objeto
+const objData = JSON.parse(jsonData);
+
+console.log(objData)
+console.log(typeof objData)
+
+objData.map((pessoa) => {
+    console.log(pessoa.nome)
+})
