598eb7ae8be2e60df5dabc4b47c47a22d1617b6d
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-04-16 13:19:46 -0300

    Exercise

---
+++ aula16ex/second/js/script.js
@@ -0,0 +1,15 @@
+let num = document.querySelector('#fnum')
+let sel = document.querySelector('#flista')
+let res = document.querySelector('#')
+const adc = document.querySelector('#adc')
+const fin = document.querySelector('#fin')
+valores = []
+
+
+adc.addEventListener("click", function() {
+   
+})
+
+fin.addEventListener("click", function() {
+
+})
