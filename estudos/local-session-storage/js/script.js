47d6bca8045338d03cd1dd3e7fab395c8d118c93
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-04-20 20:54:07 -0300

    .

--- estudos/local-session-storage/js/script.js
+++ estudos/local-session-storage/js/script.js
@@ -1,2 +1,40 @@
 // 1 - inserir dado
-localStorage.setItem("name", "Matheus")
+localStorage.setItem("name", "Matheus");
+
+// 2 - restart sem perder dados
+
+// 3 - resgatar item
+const name = localStorage.getItem("name");
+
+console.log(name);
+
+// 4 - resgate de item que não existe
+const lastName = localStorage.getItem("lastname");
+
+console.log(lastName);
+
+if(!lastName) {
+    console.log("Sem sobrenome");
+}
+
+// 5 - remover item
+localStorage.removeItem("name");
+
+// 6 - limpar todos os itens
+localStorage.setItem("a", 1);
+localStorage.setItem("b", 2);
+
+// console.log(typeof localStorage.getItem("a"));
+
+localStorage.clear();
+
+// 7 - session storage
+// sessionStorage.setItem("number", 123);
+
+// 8 - reiniciar e perder dados
+
+const n = sessionStorage.getItem("number");
+
+console.log(n);
+
+
