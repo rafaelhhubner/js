1ad3cda2b7c84d5abbf6223046b9ddb015d56b22
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-04-26 13:01:56 -0300

    Fragments

--- react/meu-projeto/src/App.js
+++ react/meu-projeto/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import SayMyName from "./components/SayMyName"
 import Pessoa from "./components/Pessoa"
 import Frase from "./components/Frase"
+import List from "./components/List"
 
 function App() {
   const nome = "Maria"
@@ -19,6 +20,7 @@ function App() {
          idade="28"
           profissao="programador"
           foto="https://via.placeholder.com/150" />
+        <List />
       </div>
   )
 }
---
+++ react/meu-projeto/src/components/Item.js
@@ -0,0 +1,10 @@
+function Item(props) {
+    return (
+    <>
+        <li>{props.marca}</li>
+        <p>TESTE</p>
+    </>
+    )
+}
+
+export default Item
