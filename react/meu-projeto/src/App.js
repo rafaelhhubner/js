733b1d11384442a10c03d2fb9356483463b85bb3
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-04-26 12:47:52 -0300

    CSS Modules

--- react/meu-projeto/src/App.js
+++ react/meu-projeto/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import HelloWorld from './components/HelloWorld'
 import SayMyName from "./components/SayMyName"
 import Pessoa from "./components/Pessoa"
+import Frase from "./components/Frase"
 
 function App() {
   const nome = "Maria"
 
   return (
-      <div className="App">  
+      <div className="App">   
+        <h1>Testando CSS</h1>
+        <Frase />
+        <Frase />
         <SayMyName nome="Matheus"/>
         <SayMyName nome="João"/>
         <SayMyName nome={nome} />
