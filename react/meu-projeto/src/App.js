0066f628a9629bfeb24a35b1d7a568cac749922a
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-04-26 14:44:46 -0300

    React Events

--- react/meu-projeto/src/App.js
+++ react/meu-projeto/src/App.js
@@ -1,9 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import SayMyName from "./components/SayMyName"
-import Pessoa from "./components/Pessoa"
-import Frase from "./components/Frase"
-import List from "./components/List"
+import Evento from "./components/Evento"
+import Form from "./components/Form"
 
 function App() {
   const nome = "Maria"
@@ -11,16 +9,9 @@ function App() {
   return (
       <div className="App">   
         <h1>Testando CSS</h1>
-        <Frase />
-        <Frase />
-        <SayMyName nome="Matheus"/>
-        <SayMyName nome="João"/>
-        <SayMyName nome={nome} />
-        <Pessoa nome="Rodrigo"
-         idade="28"
-          profissao="programador"
-          foto="https://via.placeholder.com/150" />
-        <List />
+        <Evento numero={1}/>
+        <Evento numero={2}/>
+        <Form />
       </div>
   )
 }
