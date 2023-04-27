35196e48e024bb5f2285a16e681856b79f70a1ce
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-04-26 23:05:55 -0300

    Events in Props

--- react/meu-projeto/src/components/Evento.js
+++ react/meu-projeto/src/components/Evento.js
@@ -1,12 +1,19 @@
-function Evento({ numero }) {
+import Button from "./Button"
+
+function Evento() {
 
     function meuEvento() {
-        console.log(`Opa, fui ativado! ${numero}`)
+        console.log(`Ativando primeiro evento!`)
+    }
+
+    function segundoEvento() {
+        console.log("Ativando o segundo evento!")
     }
     return (
         <div>
             <p>Clique para disparar um evento:</p>
-            <button onClick={meuEvento}>Ativar!</button>
+            <Button  event={meuEvento} text="Primeiro evento"/>
+            <Button event={segundoEvento} text="Segundo evento"/>
         </div>
     )
 }
