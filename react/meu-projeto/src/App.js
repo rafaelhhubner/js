9d47f9657645fdd7349c03543c9da87b986a2edd
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-04-27 17:04:33 -0300

    List Rendering

--- react/meu-projeto/src/App.js
+++ react/meu-projeto/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import Condicional from "./components/Condicional"
+import OutraLista from './components/OutraLista'
+
 
 
 function App() {
-  const nome = "Maria"
+  const meusItens = ['React', 'Vue', 'Angular']
 
   return (
       <div className="App">   
         <h1>Renderização Condicional</h1>
-        <Condicional />
+        <OutraLista itens={meusItens} />
+        <OutraLista itens={[]} />
       </div>
   )
 }
